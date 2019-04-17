19ca0b5ffa6d19496fc5fb4cae9e0ae109e9cf1c
fiandimas <kidaliez@gmail.com>
2019-04-17 12:15:48 +0700

    complete

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,17 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Redis;
-use DB;
+use App\Http\Controllers\Controller;
+use App\Models\User;
 
 class UserController extends Controller {
 
     public function index(){
-
+        if(Redis::get('users')){
+            return Redis::get('users');
+        }else{
+            $users = User::all();
+            $this->setRedis($users);
+            return $users;
+        }
     }
 
-    private function setUser(){
+    public function find($id){
+        if(Redis::get("user:$id")){
+            return Redis::get("user:$id");
+        }else{
+            $user = User::findOrFail($id);
+            Redis::set("user:$id", $user);
+            return $user;
+        }
+    }
 
+    private function setRedis($users){
+        Redis::set('users', $users);
+        foreach($users as $data){
+            Redis::set("user:$data->id", $data);
+        }
     }
 }
